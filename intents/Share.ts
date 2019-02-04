f9b3c6bc85ea3ef4a31bd0924d59c3e5f6389b06
Philip Hayton <gotbadger@users.noreply.github.com>
2019-02-04 12:34:16 +0000

    wip

--- intents/Share.ts
+++ intents/Share.ts
@@ -1,56 +1,48 @@
-import { FetchData, TOAUTH2AuthContext } from "@bearer/intents";
-import Client from "./client";
+import { TOAUTH2AuthContext, FetchData, TFetchActionEvent, TFetchPromise } from '@bearer/intents'
+import Client from './client'
 
+export default class SubscribeUserIntent extends FetchData implements FetchData<ReturnedData, any, TOAUTH2AuthContext> {
+    async action(event: TFetchActionEvent<TShareParams, TOAUTH2AuthContext>): TFetchPromise<ReturnedData> {
+        const channel = event.context.channel
+        try {
+            if (!channel) {
+                throw new MissingChannel()
+            }
+            if (!event.params.message) {
+                throw new MissingMessage()
+            }
+
+            const { data } = await Client(event.context.authAccess.accessToken).post(
+                `chat.postMessage`,
+                {
+                    channel: channel.id,
+                    text: event.params.message,
+                    as_user: false,
+                    parse: "full"
+                }
+            );
+
+            return { data };
+        } catch (error) {
+            return { error: error.toString() };
+        }
+    }
+}
+
+/**
+ * Typing
+ */
 type TShareParams = {
-  authId: string;
-  channelId: string;
+    message: string;
 };
+
+export type ReturnedData = any
 
 type TShareContext = {
   channel: {
     id: string;
   };
 };
-
-type TShareBody = {
-  message: string;
-};
-
-export default class ShareIntent {
-  static intentType: any = FetchData;
-
-  static async action({
-    context,
-    params
-  }: {
-    context: TOAUTH2AuthContext & TShareContext;
-    params: TShareBody;
-  }) {
-    const channel = context.channel;
-    try {
-      if (!channel) {
-        throw new MissingChannel();
-      }
-      if (!params.message) {
-        throw new MissingMessage();
-      }
-
-      const { data } = await Client(context.authAccess.accessToken).post(
-        `chat.postMessage`,
-        {
-          channel: channel.id,
-          text: params.message,
-          as_user: false,
-          parse: "full"
-        }
-      );
-
-      return { data };
-    } catch (error) {
-      return { error: error.toString() };
-    }
-  }
-}
 
 class MissingChannel extends Error {
   constructor() {
